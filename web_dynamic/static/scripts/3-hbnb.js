00eda5f69384ca23c8b6bbc6c56771b217c20f4c
Yasser Rahmouni <rahmouniyasser77@gmail.com>
2024-04-05 08:53:42 -0700

    task 4

---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,39 @@
+$(document).ready(function () {
+  const selectedAmenities = [];
+  $('input[type="checkbox"]').change(function () {
+    const amenityID = $(this).attr('data-id');
+    if ($(this).is(':checked')) {
+      if (selectedAmenities.indexOf(amenityID) === -1) {
+        selectedAmenities.push(amenityID);
+      }
+    } else {
+      const index = selectedAmenities.indexOf(amenityID);
+      if (index !== -1) {
+        selectedAmenities.splice(index, 1);
+      }
+    }
+    $('.amenities h4').text(selectedAmenities.join(', '));
+  });
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (data, textStatus) {
+    if (textStatus === 'success') {
+      if (data.status === 'OK') {
+        $('#api_status').addClass('available');
+      } else {
+        $('#api_status').removeClass('available');
+      }
+    }
+  });
+  $.ajax({
+    type: 'POST',
+    url: 'http://0.0.0.0:5001/api/v1/places_search',
+    data: '{}',
+    dataType: 'json',
+    contentType: 'application/json',
+    success: function (data) {
+      for (let i = 0; i < data.length; i++) {
+        const place = data[i];
+        $('.places ').append('<article><h2>' + place.name + '</h2><div class="price_by_night"><p>$' + place.price_by_night + '</p></div><div class="information"><div class="max_guest"><div class="guest_image"></div><p>' + place.max_guest + '</p></div><div class="number_rooms"><div class="bed_image"></div><p>' + place.number_rooms + '</p></div><div class="number_bathrooms"><div class="bath_image"></div><p>' + place.number_bathrooms + '</p></div></div><div class="description"><p>' + place.description + '</p></div></article>');
+      }
+    }
+  });
+});
